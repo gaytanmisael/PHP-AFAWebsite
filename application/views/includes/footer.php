6fd859be0964591c4b26ebcea1d15328cea9b991
Misael Gaytan <gaytanmisael@gmail.com>
2016-03-28 08:54:00 -0700

    Hosted Version

--- application/views/includes/footer.php
+++ application/views/includes/footer.php
@@ -20,11 +20,22 @@
   <script src="https://cdnjs.cloudflare.com/ajax/libs/Swiper/3.3.1/js/swiper.jquery.min.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/fancybox/2.1.5/jquery.fancybox.min.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/SlickNav/1.0.5.5/jquery.slicknav.min.js"></script>
-  <script src="<?php echo base_url() . "assets/js/scripts.js"?>"></script>
+  <script src="<?php echo base_url() . "assets/js/min/scripts-min.js"?>"></script>
 
   <?php if($main_content ==='home_page') { ?>
-  <script src="<?php echo base_url() . "assets/js/swiper.js"?>"></script>
+  <script src="<?php echo base_url() . "assets/js/min/swiper-min.js"?>"></script>
   <?php }; ?>
+
+  <script>
+    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+    })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+    ga('create', 'UA-75689505-1', 'auto');
+    ga('send', 'pageview');
+
+  </script>
 
 </body>
 </html>
